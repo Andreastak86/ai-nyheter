f67216c8684d9765467672883d24b58738d785f8
Andreastak86 <andreastak.kodehode@gmail.com>
2026-07-25 23:31:47 +0200

    feat: remove site and base configuration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://andreastak86.github.io',
-	base: '/ai-nyheter',
-});
+export default defineConfig({});
